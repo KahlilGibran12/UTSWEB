97d69cbf748844c3772371eabe25ada9505a5b01
khalil <khalilgibran869@gmail.com>
2023-05-17 01:00:54 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <nav
+        class="navbar navbar-expand-lg bg-body-tertiary-center">
+          <a>القرآن</a>
+          <router-link :class="{ active: $route.name === 'surah' }" class="y nav-link" to="/surah">Surah</router-link>
+          <router-link :class="{ active: $route.name === 'about' }" class="y nav-link" to="/about" >About Us</router-link>
+      </nav>
+      <img src="../src/assets/w1.png"/>
+    </div>
+    
+    
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+a{
+  font-size: 40px;
+}
+.y {
+  color: rgb(23, 21, 21);
+
+    align-items: center;
+    list-style: none;
+    text-decoration: none;
+}
+.navbar-expand-lg {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    text-align: center;
+    background: linear-gradient(rgba(9, 80, 231, 0.96), rgba(60, 125, 190, 0.9), rgba(218, 224, 224, 0.96)),center/cover no-repeat;
+    background-size: cover;
+    display: flex;
+    justify-content: space-between;
+    padding: 5px 10px ;
+
+}
+.y:hover {
+    color: #06060b;
+    font-size: 20px;
+    transition: .2s;
+    background-color: rgba(95, 98, 99, 0.595);
+}
+
+.active {
+  color: black !important;
+    text-align: center;
+    padding: 5px;
+}
+header{
+  text-align: center;
+  background-image: url("../src/assets/p.jpg");
+  background-size: cover;
+  
+  
+  
+}
+</style>
+
---
+++ src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<template>    
+    <main>
+        <div class="container">
+          <div class="row mt-5">
+            <div class="mt-5"> 
+              <h1>About Us</h1>
+              <p>
+                Al-Quran according to the terminology, namely the word of Allah which contains miracles which was revealed to the apostle or His Majesty the Prophet sallallaahu alaihi wa sallam the prophet Muhammad shallallahu alaihi wasallam through the angel Gabriel written in the mushab which was conveyed to us mutawatir, reading it is considered worship starting from surah al-Fatihah and ending with surah an-nas which is the Qur'an in terminology. The language of the Qur'an means that this perfect reading is Allah's perfect choice because there is not a single verse since humans know how to read or read and write can match the glorious Al-Quran, there are Koran readings that can be read by hundreds of millions of people who do not understand or unable to pass its origin and memorized by adults, adolescents and children, it forgets the definition of the Qur'an. And a book or warning that has blessings that we have sent down, why do you want it, this is a blessing for us because the Koran is a book of warnings that has blessings that we have sent down, so why do you want the term to contain the meaning of ridicule, the blessing here is a book that we sent down to you in full with blessings that they pay heed to its verses and to instruct those who have minds.
+              </p>
+            </div>
+            <div class="my-5">
+              <h1>Credits</h1>
+              <p>
+                First of All,by the grace of Allah,I have become able to create this website to make Al-Qur'an available to all the people in online. Actually,I used An API which is developed by some great developers and i also take the idea of FrontEnd of this website from Quran.com . Thanks to Quran.com's developers to give us a great API and cool Quran website.
+              </p>
+            </div>
+          </div>
+        </div>
+        <hr>
+      </main>
+</template>
+<style>
+main{
+    text-align: center;
+    background-color: rgba(85, 255, 198, 0.693);
+    background-size: cover;
+}
+</style>
